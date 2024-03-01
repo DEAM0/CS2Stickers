9a854bdfe5105147a1847dcb8e1b1df7ac9ebf4b
pc <pc@CELSIUS-W530>
2024-03-01 08:11:55 +0100

    Add project files.

---
+++ CSGOStickers/Main.cs
@@ -0,0 +1,37 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace CSGOStickers;
+public class CSGOStickers : BasePlugin
+{
+    public override string ModuleName => "Stickers Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Skript napisal damik pre StayFragged!");
+    }
+
+    [ConsoleCommand("!stickers", "Tento prikaz zobrazi menu pre stickers")] 
+    public void OnCommand(CCSPlayerController? player, CommandInfo command)
+    {
+        if (player == null)
+        {
+            Console.WriteLine("Vyber si nalepku");
+            Console.WriteLine("!1 | 360 No Scope");
+            Console.WriteLine("!2 | Abalone Strafe (Holo)");
+            Console.WriteLine("!3 | Ace Clutch Co. (Holo)");
+            Console.WriteLine("!4 | Ace Devil (Foil)");
+            Console.WriteLine("!5 | Aces High");
+            Console.WriteLine("!6 | Aces High (Holo)");
+            Console.WriteLine("!7 | Adepta Sororitas");
+            Console.WriteLine("!8 | Aeldari Avatar");
+            Console.WriteLine("!9 | Ďalšia strána");
+            return;
+        }
+    }
+}
+
+
---
+++ CSGOStickers/PlayerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOStickers
+{
+    public class PlayerInfo
+    {
+        public int Index { get; set; }
+        public int Slot { get; set; }
+        public int? UserId { get; set; }
+        public string? SteamId { get; set; }
+        public string? Name { get; set; }
+        public string? IpAddress { get; set; }
+
+
+    }
+}
